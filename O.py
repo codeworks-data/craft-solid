ea9a8ca977dfb96039a68228d2f993e6a65c8330
Edwin <edwin.oursel@codeworks.fr>
2020-06-26 14:23:41 +0200

    adding O solution

--- O.py
+++ O.py
@@ -1,14 +1,21 @@
 # Open-closed principle
 
 
-class Discount:
+class DiscountCustomer:
     def __init__(self, customer, price):
         self.customer = customer
         self.price = price
 
     def get_discount_price(self):
-        if self.customer == 'fav':
-            return self.price * 0.2
-        if self.customer == 'vip':
-            return self.price * 0.5
         return self.price
+
+
+class FavoriteDiscountCustomer(DiscountCustomer):
+    def get_discount_price(self):
+        return super().get_discount_price() * 0.8  # 20% discount
+
+
+class VIPDiscountCustomer(DiscountCustomer):
+    def get_discount_price(self):
+        return super().get_discount_price() * 0.5
+
