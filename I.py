8d07a4d6d08b866e92f42f23e7f5c543db079c9e
Edwin <edwin.oursel@codeworks.fr>
2020-07-31 16:50:35 +0200

    adding I solution

--- I.py
+++ I.py
@@ -2,24 +2,15 @@
 
 
 class Shape:
-    def draw_circle(self):
-        raise NotImplemented
-
-    def draw_square(self):
+    def draw_shape(self):
         raise NotImplemented
 
 
 class Circle(Shape):
-    def draw_circle(self):
+    def draw_shape(self):
         print("o")
-
-    def draw_square(self):
-        pass
 
 
 class Square(Shape):
-    def draw_circle(self):
-        pass
-
-    def draw_square(self):
+    def draw_shape(self):
         print("[]")
